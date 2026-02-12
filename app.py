21952bb74eddd9dc40cf30c4db044e120a018f00
Gabrielli Michachi <gabrielli.paula@aluno.senai.br>
2026-02-12 17:22:33 -0300

    MODIFY: arrumei coisas

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+import  mysql.connector
 
 app = Flask(__name__)
 
+@app.route("/")
+@app.route("/principal" , methods =["GET"])
+def pagina_principal():
 
+
+    #conectando no banco de dados
+    conexao = mysql.connector.connect(
+        host = "localhost"
+        port = 3306,
+        user = "root",
+        password = "root"
+        database = "MeowMusic"
+    )
+
+    #criando o cursor
+    cursor = conexao.cursor(dictionary=True)
+
+    #executando a consulta
+    cursor.execute("SELECT codigo, cantor, genero, nome, duracao, url_imagem FROM musica;")
+
+    #recuperando os dados
+    musicas = cursor.fetchall()
+
+    #fechando a conexao
+    conexao.close()
+        
+    return render_template("principal.html" , musicas = musicas)
 
 
 
